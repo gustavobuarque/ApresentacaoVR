b169a64ae78b9946296dbd0601b7cc07edf98b41
Gustavo Buarque Costa Cardoso <gustavobuarque@gmail.com>
2017-03-07 09:39:58 -0300

    Ajuste no som ambiente ao ir para o jardim e voltar para o museu baseado no clique no caminho e no piso. Ajuste no canvas do Vídeo. E no canvas da photosphera

---
+++ Assets/Scripts/GardenSoundControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GardenSoundControl : MonoBehaviour
+{
+    public GameObject gardenAudioHolder;
+    public GameObject museumAudioHolder;
+    public GameObject _camera;
+
+    private GvrAudioSource gardenSource;
+    private GvrAudioSource museumSource;
+
+    void Start()
+    {
+        gardenSource = gardenAudioHolder.GetComponent<GvrAudioSource>();
+        museumSource = museumAudioHolder.GetComponent<GvrAudioSource>();
+    } 
+
+    public void GardenSound()
+    {
+        Debug.Log(_camera.transform.position.y);
+        if (!gardenSource.isPlaying)
+        {
+            gardenSource.Play();
+            museumSource.Stop();
+        }
+        
+    }
+    public void MuseumSound()
+    {
+        if (!museumSource.isPlaying)
+        {
+            gardenSource.Stop();
+            museumSource.Play();
+        }
+
+    }
+}
