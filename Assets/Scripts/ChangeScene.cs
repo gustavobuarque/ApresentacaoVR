040a08c5357a3df02fd63941197185c255204546
Gustavo Buarque Costa Cardoso <gustavobuarque@gmail.com>
2017-03-07 17:55:47 -0300

    Retirei todos os códigos de mudança de cena que guardavam a posição da camera, pois não funcionou adequadamente

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,41 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
     public string _scene = "inicio";
-    public GameObject FPCamera;
-    public bool useCamera = false;
-    static Vector3 cameraPosition;
-    static Quaternion cameraRotation;
-
-    public void storeCamera()
-    {
-        // Save coordinates:
-        cameraPosition = FPCamera.gameObject.transform.position;
-        cameraRotation = FPCamera.gameObject.transform.rotation;
-        Debug.Log(cameraPosition);
-    }
-
-    public void retrieveCamera()
-    {
-        // Load coordinates:
-        Debug.Log(FPCamera.CompareTag("MainCamera"));
-        FPCamera.transform.position = cameraPosition;
-        FPCamera.transform.rotation = cameraRotation;
-    }
 
     public void GotoScene()
     {
-        if (useCamera)
-        {
-
-            storeCamera();
             SceneManager.LoadScene(_scene);
-            Debug.Log("usecamera");
-        } else
-        {
-            SceneManager.LoadScene(_scene);
-            retrieveCamera();
-            Debug.Log(cameraPosition);
-        }
     }
 
 }
