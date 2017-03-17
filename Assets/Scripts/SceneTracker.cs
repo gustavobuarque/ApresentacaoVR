90ea2ca22e5f58ef6345199df0dfeae0f28f3de5
Gustavo Buarque Costa Cardoso <gustavobuarque@gmail.com>
2017-03-17 09:58:04 -0300

    modifiquei os scripts SceneTracker e SetCameraTransform para não duplicar os objetos SceneTracker sempre que caregado a cenas.

--- Assets/Scripts/SceneTracker.cs
+++ Assets/Scripts/SceneTracker.cs
@@ -6,19 +6,24 @@ public class SceneTracker : MonoBehaviour
 {
     public GameObject _camera;
     static public bool isReturning = false;
+    static public int instanceId;
+    static public int instanceId2;
+
+    //Position
     static public float _x;
     static public float _y;
     static public float _z;
-
+    
     void Awake ()
 	{
         DontDestroyOnLoad (gameObject);
+        instanceId = gameObject.GetInstanceID(); // Get the ID of the SceneTracker Object when the object awake
 	}
 
 	public void ToggleState ()
 	{
         isReturning = true;
-        
+        instanceId2 = gameObject.GetInstanceID(); // Get the ID of the SceneTracker Object when change the scene
         _x = _camera.transform.position.x;
         _y = _camera.transform.position.y;
         _z = _camera.transform.position.z;
--- Assets/Scripts/SetCameraTransform.cs
+++ Assets/Scripts/SetCameraTransform.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class SetCameraTransform : MonoBehaviour
 {
@@ -12,7 +13,18 @@ public class SetCameraTransform : MonoBehaviour
             transform.position = new Vector3(SceneTracker._x, SceneTracker._y, SceneTracker._z);
             transform.rotation = Quaternion.Euler(0f, 120f, 0f);
             _canvasInicio.SetActive(false);
-
+            
+            //Do not allow duplicate Scenetracker Objects in the scene
+            if(SceneTracker.instanceId > SceneTracker.instanceId2)
+            {
+                Object obj = EditorUtility.InstanceIDToObject(SceneTracker.instanceId2);
+                if (!obj)
+                    Debug.LogError("No object could be found with instance id: " + SceneTracker.instanceId2);
+                else
+                    Debug.Log("Object's name: " + obj.name);
+                    SceneTracker.Destroy(obj);
+            }
+            
         }
 
     }
